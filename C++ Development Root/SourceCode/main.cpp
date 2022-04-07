9b43ba88fdfb2d4e3b20cfd2b2101271a235cced
Justin Lee <jlee@Justins-M1-MacBook-Pro.local>
2022-04-07 12:34:19 -0700

    Cleaned Up Main

--- C++ Development Root/SourceCode/main.cpp
+++ C++ Development Root/SourceCode/main.cpp
@@ -21,20 +21,6 @@
 
 int main()
 {
-
-
-/*
-  std::vector<Listing> listings;
-  ListingManager listingManager(listings);
-  
-
-  std::cout << "attempting to run createNewJob";
-  listingManager.createNewJobObj("test", "test", 1234);
-  */
-
- std::vector<Listing> listings;
- 
-
   try
   {
     auto userInterface = UI::UserInterfaceHandler::createUI();
@@ -74,51 +60,4 @@ int main()
   {
     std::cerr << "Fatal:  Uncaught (unknown type) exception\n";
   }
-
-
-  std::cout << "Available tests:\n" 
-            << "1: Post Job Listing\n"
-            << "2: Build Resume\n"
-            << "3: Manage and Monitor Logs\n"
-            << "4: Quit\n"
-            << "Enter the ssd number you would like to test: ";
-
-  char response;
-  std::cin >> response;
-
-  if (response == '1')
-  {
-    /*auto acc = Account("Justin", "0000XX", "01/01/2022 12:00 AM", "JustinLee");
-    auto session = sessionHandler();
-    bool auth = session.authenticateUser(acc, "0000XX", "JustinLee");*/
-    bool auth = true;
-    if(auth == true)
-    {
-      std::cout<<"Account Successfully Authenticated\n";
-    }
-    else
-    {
-      std::cout<<"Account Failed Authentication\n";
-    }
-
-  }
-
-  else if (response == '2')
-  {
-
-  }
-
-  else if (response == '3')
-  {
-
-  }
-  else if (response == '4')
-  {
-    
-  }
-  else
-  {
-    std::cout << response << " is not available!\n";
-  }
-
 };
